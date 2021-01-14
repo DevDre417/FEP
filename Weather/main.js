5b7bfa066ce3ef83fe24deac60143337d5b0964c
Dre Lewis <andrerjl95@gmail.com>
2021-01-14 05:05:35 -0500

    Updated

--- Weather/main.js
+++ Weather/main.js
@@ -1,14 +1,24 @@
 const api = {
   key: "90e9ff7508b850b27e1cab92541a14cc",
-  baseurl: "https://api.openweathermap.org/data/2.5"
-}
+  base: "https://api.openweathermap.org/data/2.5/"
+} 
 
 const searchBx = document.querySelector(".searchBox");
 searchBx.addEventListener('keypress', setQuery);
 
 function setQuery(evt) {
   if (evt.keyCode == 13) {
-    // getResults(searchBox.value);
+    getResults(searchBx.value);
     console.log(searchBx.value);
   }
+}
+
+function getResults(query){
+  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`).then(weather => {
+      return weather.json();
+  }).then(displayResults);
+}
+
+function displayResults(weather){
+  console.log(weather);
 }
