9c38f7b14aa8101c1c033dee9131bf9cf74ffc04
Dre Lewis <andrerjl95@gmail.com>
2021-01-14 04:31:50 -0500

    Updated

--- Weather/main.js
+++ Weather/main.js
@@ -1,3 +1,14 @@
 const api = {
-  key: "90e9ff7508b850b27e1cab92541a14cc"
+  key: "90e9ff7508b850b27e1cab92541a14cc",
+  baseurl: "https://api.openweathermap.org/data/2.5"
+}
+
+const searchBx = document.querySelector(".searchBox");
+searchBx.addEventListener('keypress', setQuery);
+
+function setQuery(evt) {
+  if (evt.keyCode == 13) {
+    // getResults(searchBox.value);
+    console.log(searchBx.value);
+  }
 }
