bfe34b8167d2860d755c234e3a43abca3e07acfe
Dre Lewis <andrerjl95@gmail.com>
2021-01-16 14:48:34 -0500

    updated

--- j/script.js
+++ j/script.js
@@ -1,22 +1,23 @@
 let todos = ['Get Groceries','Wash car','Make dinner'];
-todos.push('another todo');
 
 
 todos.forEach(function (todoTitle) {
   let element = document.createElement('div');
   element.innerText = todoTitle;
   document.body.appendChild(element);
-}
-) 
+}) ;
 
-function toUpper(param){
-    let d = [];
-    for(i=0;i<param.length;i++) {
-    let x =  param[i].toUpperCase();
-        d.push(x);
-    }
-    return d;
+function addTodo() {
+  let textbox = document.getElementById('todo-title');
+  let title = textbox.value;
+  todos.push(title);
 }
 
 
-console.log(toUpper(['asdad','asdsadas']));
+let btn = document.createElement('button')
+btn.innerText = 'in-progress';
+let oc;
+btn.onclick = oc = () => btn.innerText = 'Done';
+
+document.body.appendChild(btn);
+
