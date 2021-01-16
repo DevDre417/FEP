1717627216fcbf43b67e6ff820ba4f7a19313d4d
Dre Lewis <andrerjl95@gmail.com>
2021-01-16 15:14:33 -0500

    Updated

--- j/script.js
+++ j/script.js
@@ -15,9 +15,16 @@ function addTodo() {
 
 
 let btn = document.createElement('button')
+btn.id="todo-button";
 btn.innerText = 'in-progress';
 let oc;
 btn.onclick = oc = () => btn.innerText = 'Done';
-
 document.body.appendChild(btn);
+
+
+let newel = document.createElement('div');
+newel.innerText= '0';
+document.body.appendChild(newel);
+newel.id = 'counter';
+let newell = document.getElementById('counter');
 
