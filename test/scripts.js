7d46fb89566d0f986c2b77f605c6d8da6bde7074
Dre Lewis <andrerjl95@gmail.com>
2021-01-27 01:44:24 -0500

    Updated

--- test/scripts.js
+++ test/scripts.js
@@ -2,38 +2,34 @@
 //Model
 
 const render = () => {
-  const ser = document.getElementById('inp');
-  
+  let ser = document.getElementById('inp');
   const container = document.getElementById('todoContainer');
-
-
   const addButton = document.getElementById('addB');
-
-  let ser = ser.value.trim().split(/ +/).join(" ");
 
   //Add List Item
   addButton.onclick = () => {
-      if(!ser){
-        return
-      }else{
+
+      if(ser.value !== ''){
         let id = new Date().getTime();
 
-        const listItem = ser.value;
         const cont = document.createElement('div')
         cont.id = id;
         const deleteButton = document.createElement('button')
         deleteButton.innerText = 'Delete';
         deleteButton.style = "margin-left: 5px; margin:top: 5px;font-size: 18px; position: relative; bottom: 6px; padding: 3px;"
-        cont.innerText = listItem;
+        cont.innerText = ser.value;
         cont.appendChild(deleteButton);
         container.appendChild(cont);
     
+        ser.value = ''
+
         //Delete Button
         deleteButton.onclick = () => {
                 const x = document.getElementById(id);
                 x.innerHTML = '';
         }
       }
+      
   }
 }
 
