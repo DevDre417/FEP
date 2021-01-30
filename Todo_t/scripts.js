9ae9c966e0b6b988c91db72396a8644ef1e15fa4
Dre Lewis <andrerjl95@gmail.com>
2021-01-30 00:45:44 -0500

    Updated

--- Todo_t/scripts.js
+++ Todo_t/scripts.js
@@ -1,45 +1,27 @@
-  let ser = document.querySelector('#inp');
+  const ser = document.querySelector('#inp');
   const container = document.querySelector('#todoContainer');
   const addButton = document.querySelector('#addB');
   const clearButton = document.querySelector('#clearB');
+  
   clearButton.style.color = "red";
   //Add List Item
 
-  ser.addEventListener("keypress", enter => { if (enter.keyCode == 13) addValue(ser.value)});
+  ser.addEventListener("keypress", enter => { if (enter.keyCode == 13) addValue()});
   
 
+const addValue = () => {
+  if (/\S/.test(ser.value)) {
+  const cont = document.createElement('div');
+  const deleteButton = document.createElement('button')
+  let id = new Date().getTime();
+  cont.id = id;
+  deleteButton.innerText = 'Delete';
+  deleteButton.style = "margin-left: 5px; margin:top: 5px;font-size: 18px; position: relative; bottom: 6px; padding: 3px;"
+  cont.innerText = ser.value;
+  cont.appendChild(deleteButton);
+  container.appendChild(cont);
 
-  const render = () => {
-  addButton.onclick = () => {
-    const deleteButton = document.createElement('button')
-    let id = new Date().getTime();
-
-
-      if(ser.value !== ''){
-        const cont = document.createElement('div')
-        cont.id = id;
-        
-        deleteButton.innerText = 'Delete';
-        deleteButton.style = "margin-left: 5px; margin:top: 5px;font-size: 18px; position: relative; bottom: 6px; padding: 3px;"
-        cont.innerText = ser.value;
-        cont.appendChild(deleteButton);
-        container.appendChild(cont);
-    
-        ser.value = '';
-      }
-
-//Delete Button
-      deleteButton.onclick = () => {
-        const x = document.getElementById(id);
-        x.innerHTML = '';
-}
+  ser.value = '';
   }
 }
-
-
-render();
-
-
-
-
-
+addButton.onclick = addValue;
