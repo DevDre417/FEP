53acf9c0345ede5c10a0ee6f42ba256ad81847a7
Dre Lewis <andrerjl95@gmail.com>
2021-01-30 01:22:16 -0500

    Updated

--- Todo_t/scripts.js
+++ Todo_t/scripts.js
@@ -7,19 +7,15 @@
 
   ser.addEventListener("keypress", enter => { if (enter.keyCode == 13) addValue()});
   
-  
- //Remove List item 
-const removeValue = (item) => item.innerHTML = '';
 
 //Add List Item
 const addValue = () => {
   if (/\S/.test(ser.value)) {
   const cont = document.createElement('div');
   const deleteButton = document.createElement('button');
-  let id = new Date().getTime();
-  cont.id = id;
   deleteButton.innerText = 'x';
-  deleteButton.style = "margin-left: 5px; margin:top: 5px;font-size: 18px; position: relative; bottom: 6px; padding: 3px; color: red;"
+  deleteButton.style = "margin-left: 5px; margin:top: 5px;font-size: 18px; position: relative; bottom: 6px; padding: 3px; color: red;";
+  deleteButton.onclick = () => cont.innerHTML = '';
   cont.innerText = ser.value;
   cont.appendChild(deleteButton);
   container.appendChild(cont);
