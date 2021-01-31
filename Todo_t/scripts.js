8822820cda527dc00857005c0a46ded12a650748
Dre Lewis <andrerjl95@gmail.com>
2021-01-31 00:49:51 -0500

    Updated

--- Todo_t/scripts.js
+++ Todo_t/scripts.js
@@ -9,11 +9,13 @@
 const savedState = localStorage.getItem('ad');
 const saverino = () => localStorage.setItem('ad', container.innerHTML);
 (savedState) ? container.innerHTML = savedState: null;
+///
 
-
-///Input Listener
+//Input Listener
   ser.addEventListener("keypress", enter => {  (enter.keyCode == 13) ? addValue(): null});
-  
+///
+
+
 //Add List Item
 const addValue = () => {
   if (/\S/.test(ser.value)) {
@@ -21,25 +23,30 @@ const addValue = () => {
   const deleteButton = document.createElement('button');
   deleteButton.innerText = 'x';
   deleteButton.style = "margin-left: 5px; margin:top: 5px;font-size: 18px; position: relative; bottom: 6px; padding: 3px; color: red;";
+  ///
+
+  //Delete item
   deleteButton.onclick = () => {
     cont.innerHTML = '';
     localStorage.getItem('item')
   saverino();
     localStorage.removeItem('item');
   }
+  ///
   cont.innerText = ser.value;
   cont.appendChild(deleteButton);
   container.appendChild(cont);
   const itemerino = () => localStorage.setItem('item', cont.innerText)
   saverino();
 }
+//Clear List
   cleerButton.onclick = () => {
     container.innerHTML = '';
     localStorage.removeItem('ad')
   }
   }
   ser.value = '';
-  
+///
 addButton.onclick = addValue;
 
 
